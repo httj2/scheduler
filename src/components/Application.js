fdd2edd6d68f0eeca096dcb0fb0bf9f7e713a201
Judith Huynh <judithuynh@gmail.com>
2020-05-14 16:11:47 -0400

    removed hardcoded days and retrieved days using an API request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
+
 import DayList from "components/DayList";
-import Appointment from "components/Appointment"
+import Appointment from "components/Appointment";
 
 // import DayListItem from "components/DayListItem"
 const appointments = [
@@ -55,51 +55,44 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState(0);
+  const [days, setDays] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect(()=> {
+    axios
+      .get('http://localhost:8001/api/days')
+      .then((res)=> {
+        setDays(res.data)
+    })
+    .catch((err) => console.log('didnt work:', err));
+  }, [])
+
   const scheduleAppointments = appointments.map((appointment) => {
     return (
       <Appointment key={appointment.id} {...appointment} />
     );
   });
+  
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-    days={days}
-    day={day}
-    setDay={setDay}
-  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {scheduleAppointments}
